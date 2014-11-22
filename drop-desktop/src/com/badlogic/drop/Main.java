b68cd749c96edfd94d5483b32605bde47973a95f
Pettythug <wance1@msn.com>
2014-11-22 11:59:01 -0800

    Built game and ready to play
    Built a desktop game and am ready to try it out with everyone.

---
+++ drop-desktop/src/com/badlogic/drop/Main.java
@@ -0,0 +1,14 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
+	}
+}
