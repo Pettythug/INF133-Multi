3b8ccd66d1b8fd5c4e9e52447ad302a19373b833
Pettythug <wance1@msn.com>
2014-11-24 09:23:29 -0800

    An updated and better version of the game
    This is an updated and better version of the game. The game is broken
    down into sections a little better for understanding. This has a counter
    for keeping score.

---
+++ Bubbles-desktop/src/Bubbles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package Bubbles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import Bubbles.desktop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
